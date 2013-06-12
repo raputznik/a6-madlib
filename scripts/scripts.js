b42af0153e7d74ffecfffa8499de2f6542b1142e
Jeremy Dahl <jeremy@palebluedot.ca>
2013-06-12 13:02:48 -0700

    Experimenting with js
    Learned in Yaz's class; don't need asp or php to validate

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,25 @@
 jQuery(document).ready(function($) {
-	// Stuff to do as soon as the DOM is ready;
-	//alert('Ready and Waiting');
+
+	// alert(getParameterByName('first_name'));
+	// alert(getParameterByName('last_name'));
+	if(getParameterByName('noun') !=undefined);
+	{
+
+		console.log("we have info to show");
+		var name = getParameterByName('noun');
+		var last = getParameterByName('verb1');
+		var url = getParameterByName('image');
+
+		$('#information').html('<p1>' + name + ' went to town ' + last + '</p1>');
+		$('#information').append('<img src="' + url + '"/>')
+	}
+
 });
+
+
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+    }
