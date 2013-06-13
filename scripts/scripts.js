0a65bd91b630ba18fc0eba19f1f7e81fd16d5fdf
Jeremy Dahl <jeremy@palebluedot.ca>
2013-06-13 10:33:09 -0700

    Added return-false to prevent text disappearing
    Also starting to populate different variables.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,11 +4,15 @@ $(document).ready(function(){
 	$('#button').click(function(event) {
 
 		var noun1 = $('#noun1').val();
+		var noun2 = $('#noun2').val();
+		var verb1 = $('#verb1').val();
 
 		$('#nounplace1').append(noun1);
+		$('#verblace1').append(verb1);
+		$('#nounplace2').append(noun2);
 
 		$("#main").show();
-		$("#container").hide();
+		$("#form1").hide();
 		return false;
 
 
