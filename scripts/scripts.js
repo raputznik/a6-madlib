876aba80c49ad166f9393cbefe6aff2739c9a20f
Jeremy Dahl <jeremy@palebluedot.ca>
2013-06-13 11:27:39 -0700

    Assigned variables to all my sentence components
    I think I might have figured it out...

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,12 +4,18 @@ $(document).ready(function(){
 	$('#button').click(function(event) {
 
 		var noun1 = $('#noun1').val();
-		var noun2 = $('#noun2').val();
 		var verb1 = $('#verb1').val();
+		var noun2 = $('#noun2').val();
+		var mood1 = $('#mood1').val();
+		var verb2 = $('#verb2').val();
+		var noun3 = $('#noun3').val();
 
 		$('#nounplace1').append(noun1);
-		$('#verblace1').append(verb1);
+		$('#verbplace1').append(verb1);
 		$('#nounplace2').append(noun2);
+		$('#moodplace1').append(mood1);
+		$('#verbplace2').append(verb2);
+		$('#nounplace3').append(noun3);
 
 		$("#main").show();
 		$("#form1").hide();
